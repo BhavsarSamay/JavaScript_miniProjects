d39fd158392dea29a68673cd2b5d01606eddefc3
BhavsarSamay <130301911+BhavsarSamay@users.noreply.github.com>
2024-06-14 17:02:02 +0530

    Update color.js

--- Color_picker/color.js
+++ Color_picker/color.js
@@ -1,21 +1,25 @@
-let background=document.getElementById("bg_container")
-function color1()
-{
-    let color=document.getElementById("button1").textContent
-    background.style.backgroundColor=color
+let background = document.getElementById("bg_container")
+let hexCode=document.getElementById("selectedColorHexCode")
+function color1() {
+    let color = document.getElementById("button1").textContent
+    background.style.backgroundColor = "#e0e0e0"
+    hexCode.textContent=color
 }
-function color2()
-{
-    let color=document.getElementById("button2").textContent
-    background.style.backgroundColor=color
+
+function color2() {
+    let color = document.getElementById("button2").textContent
+    background.style.backgroundColor = color
+    hexCode.textContent=color
 }
-function color3()
-{
-    let color=document.getElementById("button3").textContent
-    background.style.backgroundColor=color
+
+function color3() {
+    let color = document.getElementById("button3").textContent
+    background.style.backgroundColor = color
+    hexCode.textContent=color
 }
-function color4()
-{
-    let color=document.getElementById("button4").textContent
-    background.style.backgroundColor=color
+
+function color4() {
+    let color = document.getElementById("button4").textContent
+    background.style.backgroundColor = color
+    hexCode.textContent=color
 }
