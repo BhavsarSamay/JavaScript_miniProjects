509a99cc543ba6101e2bfc9d488ffaf6a4e98066
BhavsarSamay <130301911+BhavsarSamay@users.noreply.github.com>
2024-06-14 16:49:23 +0530

    Add files via upload

---
+++ Color_picker/color.js
@@ -0,0 +1,21 @@
+let background=document.getElementById("bg_container")
+function color1()
+{
+    let color=document.getElementById("button1").textContent
+    background.style.backgroundColor=color
+}
+function color2()
+{
+    let color=document.getElementById("button2").textContent
+    background.style.backgroundColor=color
+}
+function color3()
+{
+    let color=document.getElementById("button3").textContent
+    background.style.backgroundColor=color
+}
+function color4()
+{
+    let color=document.getElementById("button4").textContent
+    background.style.backgroundColor=color
+}
