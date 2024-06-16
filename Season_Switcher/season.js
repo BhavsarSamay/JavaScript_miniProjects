10388560a2bb315464bc1be63456b7e3542697c0
BhavsarSamay <130301911+BhavsarSamay@users.noreply.github.com>
2024-06-16 23:41:12 +0530

    Add files via upload

---
+++ Season_Switcher/season.js
@@ -0,0 +1,26 @@
+let img = document.getElementById("seasonSmallImage")
+let img1 = document.getElementById("seasonMediumImage")
+
+function spring() {
+    img.src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-spring-xs-img.png"
+    img1.src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-spring-md-img.png"
+
+}
+
+function summer() {
+    img.src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-summer-xs-img.png"
+    img1.src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-summer-md-img.png"
+
+}
+
+function autumn() {
+    img.src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-autumn-md-img.png"
+    img1.src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-autumn-md-img.png"
+
+}
+
+function winter() {
+    img.src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-winter-xs-img.png"
+    img1.src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-winter-md-img.png"
+
+}
